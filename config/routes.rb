b5527f15d8ba42855e5a6a19e5bd4f6472887f94
Ubuntu <ubuntu@ip-172-31-8-100.eu-central-1.compute.internal>
2022-05-19 17:15:11 +0000

    Adding controllers and route to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
