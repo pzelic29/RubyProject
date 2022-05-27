ea8bdf52033b9d32d31ac40bd30bae32cc68f31d
Ubuntu <ubuntu@ip-172-31-8-100.eu-central-1.compute.internal>
2022-05-27 09:56:28 +0000

    fixing naming issue and attempt to write RSpec tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
      @articles = Article.paginate(page: params[:page], per_page: 2)
-     @last5 = Article.last(5)
+     @last_five_articles = Article.last(5)
   end
 
   # GET /articles/1
